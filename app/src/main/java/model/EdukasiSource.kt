2116ef81d962fbaae4cd768bca600aad010e05dd
mourenemillie <lifeasnanaw@gmail.com>
2026-04-17 12:07:37 +0700

    TUGAS LKP 2

---
+++ app/src/main/java/model/EdukasiSource.kt
@@ -0,0 +1,33 @@
+package com.example.prak_mobile_2407051030.model
+
+import com.example.prak_mobile_2407051030.R
+
+object EdukasiSource {
+
+    val listEdukasi = listOf(
+
+        Edukasi(
+            "Kambing Jawarandu",
+            "Kambing Jawarandu adalah hasil persilangan kambing Kacang dan PE. Memiliki daya tahan tinggi dan cocok untuk peternakan rakyat.",
+            R.drawable.kambing_jawarandu
+        ),
+
+        Edukasi(
+            "Kambing Peranakan Etawa (PE)",
+            "Kambing PE dikenal sebagai penghasil susu yang baik. Tubuh besar dan telinga panjang menjuntai.",
+            R.drawable.kambing_pe
+        ),
+
+        Edukasi(
+            "Kambing Kacang",
+            "Kambing lokal Indonesia dengan ukuran kecil, tahan penyakit, dan mudah beradaptasi.",
+            R.drawable.kambing_kacang
+        ),
+
+        Edukasi(
+            "Pakan Kambing",
+            "Pakan utama kambing berupa hijauan seperti rumput dan daun, ditambah konsentrat untuk nutrisi tambahan.",
+            R.drawable.pakan
+        )
+    )
+}
---
+++ app/src/main/java/ui/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.prak_mobile_2407051030.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    TampilEdukasi()
+}
